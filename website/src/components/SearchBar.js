38c7363b7ace779d02ce6d6f895aadef0ca7a364
You Song Hou <yousonghou@yahoo.com>
2020-05-23 20:27:17 -0400

    add some image

--- website/src/components/SearchBar.js
+++ website/src/components/SearchBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { FormControl, FilledInput, InputAdornment, IconButton } from '@material-ui/core';
+import { CardMedia, FormControl, FilledInput, InputAdornment, IconButton } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles({
     root: {
-        height: "89vh"
+        height: "92vh",
+        overflow: "scroll"
     },
-    marginTop:{
-        marginTop: "10rem"
+    marginBottom:{
+        marginBottom: "1.5rem"
     }
 });
 
@@ -16,20 +17,46 @@ const SearchBar = () => {
     const classes = useStyles();
 
     return(
-        <FormControl className={classes.root} fullWidth margin="normal">
-            <FilledInput
-                autoComplete="happy"
-                placeholder="Search for Park"
-                endAdornment={
-                    <InputAdornment position="end">
-                        <IconButton type="submit"  aria-label="search">
-                            <SearchIcon />
-                        </IconButton>
-                    </InputAdornment>
-                }
-        />
-            
-        </FormControl>
+        <div className={classes.root}>
+            <FormControl  fullWidth margin="normal">
+                <FilledInput
+                    autoComplete="happy"
+                    placeholder="Search for Park"
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton type="submit"  aria-label="search">
+                                <SearchIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    }
+            />
+            </FormControl>
+            <CardMedia
+                className={classes.marginBottom}
+                component="img"
+                height="200"
+                image="https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+                title="Park" />
+            <CardMedia
+                className={classes.marginBottom}
+                component="img"
+                height="200"
+                image="https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+                title="Park" />
+            <CardMedia
+                className={classes.marginBottom}
+                component="img"
+                height="200"
+                image="https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+                title="Park" />
+            <CardMedia
+                className={classes.marginBottom}
+                component="img"
+                height="200"
+                image="https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+                title="Park" />
+        </div>
+        
     )
 }
 
