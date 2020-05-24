33fadbb5741a2d75e2a768c915b90f45ed8d0abd
nathaliavenerayan <47015176+nathaliavenerayan@users.noreply.github.com>
2020-05-24 00:43:20 -0400

    Revert "refactor map.js"

--- website/src/components/Map.js
+++ website/src/components/Map.js
@@ -3,8 +3,6 @@ import { Map, Marker, TileLayer, Popup } from 'react-leaflet';
 import { geolocated } from "react-geolocated";
 import L from 'leaflet';
 
-import data from './Data.json';
-
 import IconBlue from '../images/icon1.svg';
 import IconRed from '../images/icon2.svg';
 import IconHome from '../images/home.svg';
@@ -74,17 +72,16 @@ class MapView extends Component{
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    {data.map((park, index) => {
-                        return(
-                            <Marker key={index} position={park.coords} icon={park.amount > 50 ? redIcon : blueIcon}>
-                                <Popup>
-                                    {park.name}<br />
-                                    <strong>{park.amount} People</strong>
-                                </Popup>
-                            </Marker>
-                        )
-                    })}
-                    
+                    <Marker position={[40.0423477, -100.4082212]} icon={blueIcon}>
+                        <Popup>
+                            10/50
+                        </Popup>
+                    </Marker>
+                    <Marker position={[30.0423477, -100.4082212]} icon={redIcon}>
+                        <Popup>
+                            100/75
+                        </Popup>
+                    </Marker>
                     {location}
                 </Map>
             </div>
